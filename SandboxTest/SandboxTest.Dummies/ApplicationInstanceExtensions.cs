d04f626e25bcb37c03605c35cc7b22426609f0f8
DESKTOP-Q7911QU\alecs <alecu89@yahoo.com>
2024-04-03 21:09:04 +0300

    added dummy runner and dummy controller for tests more like unit tests

---
+++ SandboxTest/SandboxTest.Dummies/ApplicationInstanceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using SandboxTest.Dummies;
+
+namespace SandboxTest.WireMock
+{
+    public static class ApplicationInstanceExtensions
+    {
+        /// <summary>
+        /// Assigns a <see cref="DummyApplicationRunner"/> as the runner to the application instance.
+        /// </summary>
+        /// <param name="applicationInstance"></param>
+        /// <returns></returns>
+        public static IApplicationInstance UseDummyApplicationRunner(this IApplicationInstance applicationInstance)
+        {
+            applicationInstance.UseRunner(new DummyApplicationRunner());
+            return applicationInstance;
+        }
+
+        /// <summary>
+        /// Adds an application controller of type <see cref="DummyApplicationController"/> to the given application instance.
+        /// </summary>
+        /// <param name="applicationInstance"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static IApplicationInstance AddDummyApplicationController(this IApplicationInstance applicationInstance, string? name = default)
+        {
+            var dummyApplicationRunner = applicationInstance.Runner as DummyApplicationRunner;
+            if (dummyApplicationRunner == null)
+            {
+                throw new InvalidOperationException("Invalid application runner configured on application instance, expected DummyApplicationController");
+            }
+
+            var dummyApplicationController = new DummyApplicationController(name);
+            applicationInstance.AddController(dummyApplicationController);
+            return applicationInstance;
+        }
+    }
+}
---
+++ SandboxTest/SandboxTest.Dummies/DummyApplicationController.cs
@@ -0,0 +1,59 @@
+﻿namespace SandboxTest.Dummies
+{
+    /// <summary>
+    /// Dummy application controller class used more for test closer to unit tests rather than sandbox or integration tests.
+    /// </summary>
+    public class DummyApplicationController : IApplicationController
+    {
+        private readonly string? _name;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DummyApplicationController"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public DummyApplicationController(string? name) 
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// Returns the name of the dummy controller
+        /// </summary>
+        public string? Name { get => _name; }
+
+        /// <summary>
+        /// Just returns Task.Completed without doing anything else.
+        /// </summary>
+        /// <param name="applicationInstance">Not actually used</param>
+        /// <returns></returns>
+        public Task BuildAsync(IApplicationInstance applicationInstance)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Just returns Task.Completed without doing anything else.
+        /// </summary>
+        /// <param name="applicationInstance">Not actually used</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Throws invalid operation exception if it's not used with dummy application runner.</exception>
+        public Task ConfigureBuildAsync(IApplicationInstance applicationInstance)
+        {
+            if (applicationInstance.Runner is not DummyApplicationRunner)
+            {
+                throw new InvalidOperationException("Dummy application controller can only be used with dummy application runner");
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Just returns Task.Completed without doing anything else.
+        /// </summary>
+        /// <param name="applicationInstance">Not actually used</param>
+        /// <returns></returns>
+        public Task ResetAsync(ApplicationInstance applicationInstance)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
